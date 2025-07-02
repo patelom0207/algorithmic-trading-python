5cf3d059636ddff49166ca6e36f828230a4a08c7
Om Patel <patelom0207@gmail.com>
2025-07-01 22:25:20 -0500

    Acquired IEX Cloud API Token

--- equal_weight_S&P_500.py
+++ equal_weight_S&P_500.py
@@ -6,8 +6,9 @@ import xlsxwriter
 import math
 
 #Importing the S&P 500 stocks list
-file_path = "C:\Users\patel_udh9l4n\Downloads\S&P 500 Companies (Standard and Poor 500) - basics.csv"
+file_path = "C:/Users/patel_udh9l4n/OneDrive/algorithmic-trading-python/S&P_500_companies.csv"
 stocks = pd.read_csv(file_path)
 
 #Acquiring API Token
-from secrets import IEX_CLOUD_API_TOKEN
+from .secrets import IEX_CLOUD_API_TOKEN
+
