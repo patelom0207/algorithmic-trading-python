92cbde544d87a44f8cd4919868995135f9b2a1a7
Om Patel <patelom0207@gmail.com>
2025-07-02 22:49:44 -0500

    Making first API call

--- equal_weight_S&P_500.py
+++ equal_weight_S&P_500.py
@@ -12,3 +12,7 @@ stocks = pd.read_csv(file_path)
 #Acquiring API Token
 from .secrets import IEX_CLOUD_API_TOKEN
 
+#Making first API call
+symbol = "AAPL"
+api_url = f'https://sandbox.iexapis.com/stock/{symbol}/quote/?tokan={IEX_CLOUD_API_TOKEN}'
+data = requests.get(api_url).json()
